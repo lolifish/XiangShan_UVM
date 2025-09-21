f825319569de90cd704beb6aded8d347cedc1c59
lolifish <lolifish0716@outlook.com>
2025-09-21 14:42:50 +0800

    🐛fix: Improve model logic

--- model_waylookup.py
+++ model_waylookup.py
@@ -45,24 +45,25 @@ class UpdateData:
 class WaylookupModel:
     def __init__(self, depth=32):
         self.depth = depth
+        self.flush()
+    
+    def flush(self):
+        """刷新"""
         # 指针信息
         self.read_ptr = PtrData()
         self.write_ptr = PtrData()
         self.gpf_ptr = PtrData()
-
         # Entries 环形队列
         self.entries = [EntryData() for _ in range(self.depth)]
-
         # 队列空/满
         self.empty
         self.full
-
         # gpf数据
         self.gpf_data = GpfData()
         self._gpf_valid = False
         self.gpf_hit
 
-    def read(self, bypass: bool=False, bypass_data: Optional[EntryData]=None) -> Tuple[bool, Optional[EntryData], bool, Optional[GpfData]]:
+    def read(self) -> Tuple[bool, Optional[EntryData], bool, Optional[GpfData]]:
         """Waylookup读入数据
         bool: 读是否成功
         EntryData: 读到的数据
@@ -70,14 +71,8 @@ class WaylookupModel:
         GpfData: GPF数据
         """
         # 无法在不Bypass的情况下读取空队列
-        if self.empty and not bypass:
+        if self.empty :
             return False, None, False, None
-        # 如果Bypass就先写，注意不触发gpf
-        if bypass:
-            # Bypass只在队列为空时可用
-            if not self.empty:
-                raise IndexError("Use bypass read while pipeline is not empty.")
-            self.write(bypass_data, gpf_en=False)
         # 读数据
         data: EntryData = self.entries[self.read_ptr.value]
         # 是否GPF_hit
@@ -96,21 +91,18 @@ class WaylookupModel:
         return True, data, gpf_hit, gpf_data
         
 
-    def write(self, data: EntryData, gpf_data: GpfData ,gpf_en: bool=True) -> bool:
+    def write(self, data: EntryData, gpf_data: GpfData) -> bool:
         """Waylookup写入数据"""
         # 如果队列满了则拒绝写
         if self.full:
             return False
-        # 错误情况：gpf_en为False但gpf_valid有效（即错误的bypass读）
-        if (self._gpf_valid and not gpf_en):
-            raise RuntimeError("Impossible to do bypass read when gpf is enable.")
         # 如果GPF_Valid有效则拒绝写
         if self._gpf_valid:
             return False
         # 写数据
         self.entries[self.write_ptr.value] = data
         # 检查gpf
-        if (gpf_en and (data.itlb_exception_0 or data.itlb_exception_1)):
+        if (data.itlb_exception_0 or data.itlb_exception_1):
             self._gpf_valid = True
             self.gpf_data = gpf_data
             self.gpf_ptr = self.write_ptr
@@ -120,6 +112,25 @@ class WaylookupModel:
             self.write_ptr.value = 0
             self.write_ptr.flag ^= 1
         return True
+    
+    def bypass(self, data: EntryData, gpf_data: GpfData) -> Tuple[bool, Optional[EntryData], bool, Optional[GpfData]]:
+        """Bypass读的特殊情况"""
+        # 是否满足Bypass读的条件
+        if not self.empty:
+            raise IndexError("Can't bypass read while pipeline is not empty.")
+        # 更新读写指针
+        self.read_ptr.value += 1
+        if (self.read_ptr.value >= self.depth):
+            self.read_ptr.value = 0
+            self.read_ptr.flag ^= 1
+        self.write_ptr.value += 1
+        if (self.write_ptr.value >= self.depth):
+            self.write_ptr.value = 0
+            self.write_ptr.flag ^= 1
+        # 如果Bypass存在gpf信息，不使能gpf_valid
+        have_gpf = data.itlb_exception_0 or data.itlb_exception_1
+        # 直接返回数据
+        return True, data, have_gpf, gpf_data
     
     def update(self, update_data: UpdateData) -> bool:
         hit = False
